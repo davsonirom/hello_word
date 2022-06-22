24c0d1d9fb8fe3dcdbfd7a049dd96cee1d9ae715
Davson Oldac Damasceno Machado <davsonirom@me.com>
2022-06-22 00:13:23 -0300

    const e construtor

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HelloWorkPage(),
     );
   }
 }
 
 class HelloWorkPage extends StatelessWidget{
+  const HelloWorkPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context){
-    return Center(child: Text('Hello Word Flutterando!'),);
+    return const Material(child: Center(child: Text('Hello Word Flutterando!'),));
   }
 }
